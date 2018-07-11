8dd563f18f30c17125207860a735f21ec2c0675e
DeusVultron <37828829+DeusVultron@users.noreply.github.com>
2018-07-11 18:28:47 +1000

    Create player object.py

---
+++ player object.py
@@ -0,0 +1,17 @@
+class player:
+
+    def __init__ (self, name, runs, balls, wickets, b_overs, b_runs, maiden, team):
+        self.name = name
+        self.runs = runs
+        self.balls = balls
+        self.wickets = wickets
+        self.b_overs = b_overs
+        self.b_runs = b_runs
+        self.maidens = maiden
+        self.team = team
+        
+players = {}
+
+while input("what names of player: "):
+    
+    pass
